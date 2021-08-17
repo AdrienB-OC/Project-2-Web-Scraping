085b4c701ce38547757427dea7c6e7f5ae7b3a80
Adrien Burgun <adrienburgun@hotmail.fr>
2021-08-17 23:29:55 +0900

    Modification du code via l'ajout de listes pour plus de lisibilité

--- projet-2.py
+++ projet-2.py
@@ -6,9 +6,9 @@ home = 'http://books.toscrape.com/index.html'
 response = requests.get(home)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-multiplePages = False
+
 catLink = soup.find('div', {'class': 'side_categories'})
-ligne = 1
+
 for li in catLink.find_all('li'):
 
     b = li.find('a')
@@ -20,84 +20,62 @@ for li in catLink.find_all('li'):
         pass
 
     else:
-        for i in range(1,10):
+        count = 0
+        url_list = []
+
+        for i in range(1, 10):
+
             if i == 1:
                 print(catName)
                 print(lien)
+                url_list.append(lien)
+                count += 1
 
             elif i == 2 and (requests.get(lien.replace('index.html', 'page-' + str(i) + '.html')).ok):
-                lien = lien.replace('index.html', 'page-' + str(i) + '.html')
-                print(lien)
-            elif i > 2 and (requests.get(lien.replace('page-2.html', 'page-' + str(i) + '.html')).ok):
-                lienB = lien.replace('page-2.html', 'page-' + str(i) + '.html')
-                print(lienB)
-                multiplePages = True
-            else:
-                multiplePages = False
+                url_list.append(lien.replace('index.html', 'page-' + str(i) + '.html'))
+                count += 1
 
+            elif i > 2 and (requests.get(lien.replace('index.html', 'page-' + str(i) + '.html')).ok):
+                url_list.append(lien.replace('index.html', 'page-' + str(i) + '.html'))
+                count += 1
+
+            else:
                 break
 
 
 
-            with open(str(catName) + '.csv', 'w', encoding="utf-8") as outf:
-                outf.write('url, title, upc, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url \n')
 
+        with open(str(catName) + '.csv', 'w', encoding="utf-8") as outf:
+            outf.write('url, title, upc, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url \n')
 
+            for j in range(0, count):
+                page = url_list[j]
+                # page = 'http://books.toscrape.com/catalogue/category/books/mystery_3/page-' + str(i) + '.html'
+                response = requests.get(page)
 
-                for j in range(1, 10):
+                if response.ok:
+                    soup2 = BeautifulSoup(response.content, 'html.parser')
 
+                    # Find all book links on that page
+                    liens = soup2.find_all('h3')
 
-                    if j == 1:
-                        page = lien
-                        response = requests.get(page)
-                    elif j == 2 and i == 2 and (requests.get(lien.replace('index.html', 'page-' + str(i) + '.html')).ok):
-                        page = lien
-                        response = requests.get(page)
-                    elif multiplePages and i > 2 and j > 2:
-                        page = lienB
-                        response = requests.get(page)
-                    else:
-                        response = requests.get('http://books.toscrape.com/index8.html')
-                        break
-                    #page = 'http://books.toscrape.com/catalogue/category/books/mystery_3/page-' + str(i) + '.html'
+                    pages_details = ""
 
+                    for k in liens:
+                        a = k.find('a')
+                        url = a['href']
+                        url = url.replace('../../../', 'http://books.toscrape.com/catalogue/')
 
+                        response2 = requests.get(url)
 
-                    if response.ok:
+                        if response2.ok:
+                            soup3 = BeautifulSoup(response2.content, 'html.parser')
+                            # Get all data
+                            all_data = get_all_data(soup3)
+                            # Download image
 
-                        soup2 = BeautifulSoup(response.content, 'html.parser')
-                        liens = soup2.find_all('h3')
-
-                        pages_details = ""
-
-                        for lienC in liens:
-                            a = lienC.find('a')
-                            url = a['href']
-                            url = url.replace('../../../', 'http://books.toscrape.com/catalogue/')
-
-
-                            response2 = requests.get(url)
-                            if response2.ok:
-                                soup3 = BeautifulSoup(response2.content, 'html.parser')
-
-                                # Get all data
-                                all_data = get_all_data(soup3)
-
-                                # Test
-                                #print('Link : ' + url)
-                                #print('Title : ' + title)
-                                #print('UPC : ' + upc)
-                                #print('Price excluding tax : ' + price)
-                                #print('Price including tax : ' + pricetax)
-                                #print('Number available : ' + availability)
-                                #print('Product description : ' + product_description)
-                                #print('Image link : ' + imglink)
-                                #print('Category : ' + category)
-                                #print(review_rating)
-                                pages_details = f"{url},{all_data}\n'"
-                                #Output CSV
+                            pages_details = f"{url},{all_data}\n'"
                             outf.write(pages_details)
 
-                           # else:
-                                #break
+
 
